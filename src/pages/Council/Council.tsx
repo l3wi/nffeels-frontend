2e028e5a262d74c3bca7b985eec8305a6734b778
Lewis Freiberg <1716917+l3wi@users.noreply.github.com>
2021-10-02 14:04:54 +0200

    Add wojak council scaffolding

--- src/pages/Council/Council.tsx
+++ src/pages/Council/Council.tsx
@@ -1,7 +1,53 @@
 import React from "react"
+import styles from "./Council.module.scss"
+import Lines from "components/Lines/Lines"
 
 const Council = () => {
-  return <h1>Wojak Council Here!</h1>
+  const proposals = [{ status: "executed" }, { status: "queued" }, { status: "pending" }]
+  return (
+    <div>
+      <h1 className={styles.pageTitle}>Council of Wojaks</h1>
+      <div className={styles.contentSection}>
+        <div className={styles.votingBody}>
+          <div className={styles.linesContainer}>
+            <Lines />
+            Voting Power
+            <Lines />
+          </div>
+          <div className={styles.innerContent}>
+            <div className={styles.wojakBalance}>
+              <h3>wojak balance</h3>
+              <div>
+                <span>Wojaks</span>
+                <span>12</span>
+              </div>
+            </div>
+            <button className={styles.getStarted}>get started</button>
+          </div>
+        </div>
+
+        <div className={styles.proposalBody}>
+          <div className={styles.linesContainer}>
+            <Lines />
+            Proposals
+            <Lines />
+          </div>
+          <div className={styles.innerContent}>
+            {proposals.map((prop, i) => (
+              <div className={styles.proposal}>
+                <h3>Proposal #{i}</h3>
+                <div className={prop.status === "executed" ? styles.inverted : ""}>
+                  {prop.status}
+                </div>
+                <button>view</button>
+              </div>
+            ))}
+            <div>all proposals</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Council
